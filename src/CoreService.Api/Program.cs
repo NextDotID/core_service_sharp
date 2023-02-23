8e2ac196e1139e94c6d0767a2f62a6462ea428c5
Mitch Hwang <mitchx7@outlook.com>
2023-02-23 20:35:35 +0800

    REFACTOR!: remove blazor web hosting

--- src/CoreService.Api/Program.cs
+++ src/CoreService.Api/Program.cs
@@ -8,45 +8,37 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .Configure<RouteOptions>(options => options.LowercaseUrls = true)
-    .AddControllersWithViews()
+    .AddControllers()
     .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
-builder.Services.AddHttpClient();
-builder.Services.AddRazorPages();
-builder.Services.AddProblemDetails();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
+builder.Services.AddProblemDetails();
 
 builder.Services.AddLiteDb();
+builder.Services.AddHttpClient();
 builder.Services.AddTransient<IVault, DatabaseVault>();
 builder.Services.AddTransient<IAgent, DockerComposeAgent>();
 builder.Services.AddTransient<Injector>();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseExceptionHandler();
-app.UseStatusCodePages();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseWebAssemblyDebugging();
     app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-app.UseRouting();
-app.MapRazorPages();
 app.MapControllers();
-app.MapFallbackToFile("index.html");
 
 app.Run();
